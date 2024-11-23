2d074df7771d6e984ab936db7a65139497a7fd55
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-23 14:23:53 -0600

    Create a category use case to get categories

---
+++ servi-connect/src/logic/usecases/category.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { StandardApiResponse } from '../types'
+import { Category } from '../entities'
+import { API_URL } from 'config'
+import { toast } from 'sonner'
+
+export const getCategories = async () => {
+  try {
+    const response = await axios.get<StandardApiResponse<Category[]>>(
+      `${API_URL}/categories`
+    )
+    const { isSuccess, data: responseData, error } = response.data
+
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to get categories'
+    toast.error(fallbackError)
+    return {
+      isSuccess: false,
+      data: null,
+      error: fallbackError
+    }
+  }
+}
