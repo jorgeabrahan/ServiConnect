ffa4695f57301e68343183e92769ab54e6b834da
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-22 20:29:08 -0600

    Setup sonner toaster in layout

--- serviconnect/src/app/layout.tsx
+++ serviconnect/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './styles/global.css'
 import { Header } from '@/components'
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <Header />
         {children}
+        <Toaster richColors />
       </body>
     </html>
   )
