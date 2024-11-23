e736aaf97951b529242d0d9dd419f2e2989c3a4f
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-22 20:27:46 -0600

    Setup alias imports properly in nextjs project

--- serviconnect/next.config.ts
+++ serviconnect/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks']
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src')
+    }
+    return config
   }
 }
 
