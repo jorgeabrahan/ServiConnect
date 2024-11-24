d507e6e05aa2158369098ebc18191b5a7e047159
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-24 14:14:46 -0600

    Create a use category to get a single category by id

--- servi-connect/src/logic/usecases/category.ts
+++ servi-connect/src/logic/usecases/category.ts
@@ -27,3 +27,22 @@ export const getCategories = async () => {
     }
   }
 }
+
+export const getCategoryById = async (id: string) => {
+  try {
+    const response = await axios.get<StandardApiResponse<Category>>(
+      `${API_URL}/categories/${id}`
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to get category'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
