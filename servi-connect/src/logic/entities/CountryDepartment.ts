6b5e65c285243e645354fe854f5d00c71719890b
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-25 07:08:35 -0600

    Change the country department city interface to better adjust to the project requirements

--- servi-connect/src/logic/entities/CountryDepartment.ts
+++ servi-connect/src/logic/entities/CountryDepartment.ts
@@ -3,5 +3,5 @@ import { CountryDepartmentCity } from './CountryDepartmentCity'
 export interface CountryDepartment {
   id: string
   title: string
-  cities: CountryDepartmentCity[]
+  cities?: CountryDepartmentCity[]
 }
