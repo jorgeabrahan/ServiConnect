668b2717bbaa342f97fcfda49b159498d9707ffe
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-25 01:10:31 -0600

    Add a flag property to the categories store to determine when categories have been fetched

--- servi-connect/src/logic/stores/storeCategories.ts
+++ servi-connect/src/logic/stores/storeCategories.ts
@@ -2,11 +2,14 @@ import { create } from 'zustand'
 import { Category } from '../entities'
 
 interface StoreCategories {
+  isAlreadyFetched: boolean
   categories: Category[]
   setCategories: (categories: Category[]) => void
 }
 
 export const storeCategories = create<StoreCategories>((set) => ({
+  isAlreadyFetched: false,
   categories: [],
-  setCategories: (categories: Category[]) => set({ categories })
+  setCategories: (categories: Category[]) =>
+    set({ categories, isAlreadyFetched: true })
 }))
