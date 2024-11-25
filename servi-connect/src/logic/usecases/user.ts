b8af2247dbc371cf1d43c549c234ab00cee07e67
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-25 07:05:27 -0600

    Create user module use cases with methods to get, create, and update a user address

---
+++ servi-connect/src/logic/usecases/user.ts
@@ -0,0 +1,90 @@
+import axios from 'axios'
+import { StandardApiResponse } from '../types'
+import { UserAddressDto, UserAddress } from '../entities/UserAddress'
+import { API_URL } from 'config'
+import { toast } from 'sonner'
+
+export const getUserAddress = async (userId: string, token: string) => {
+  try {
+    const response = await axios.get<StandardApiResponse<UserAddress>>(
+      `${API_URL}/users/${userId}/address`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to get address'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
+
+export const createAddress = async (
+  userId: string,
+  address: UserAddressDto,
+  token: string
+) => {
+  try {
+    const response = await axios.post<StandardApiResponse<UserAddress>>(
+      `${API_URL}/users/${userId}/address`,
+      address,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    toast.success('Address created successfully')
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to create address'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
+
+export const updateAddress = async (
+  userId: string,
+  addressId: string,
+  address: UserAddressDto,
+  token: string
+) => {
+  try {
+    const response = await axios.patch<StandardApiResponse<UserAddress>>(
+      `${API_URL}/users/${userId}/address/${addressId}`,
+      address,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    toast.success('Address updated successfully')
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to update address'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
