ddc333683a5a8f2d6cbcf26deed655b56b4a86f5
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-25 01:09:22 -0600

    Implement header action components and validations to show user icon when authenticated

--- servi-connect/src/components/Header/index.tsx
+++ servi-connect/src/components/Header/index.tsx
@@ -1,64 +1,21 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { Button, Dialog, DialogPanel, PopoverGroup } from '@headlessui/react'
 import {
-  Button,
-  Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel
-} from '@headlessui/react'
-import {
-  ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  SquaresPlusIcon,
   UserCircleIcon,
   WrenchScrewdriverIcon,
   XMarkIcon
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { storeAuth, storeModalAuth } from '@/logic/stores'
 import { CxButton } from '../Interactions'
-import { getCategories } from '@/logic/usecases'
+import { useCategories } from '@/logic/hooks'
+import { HeaderActionPopover } from './HeaderActionPopover'
+import { HeaderActionDisclosure } from './HeaderActionDisclosure'
+import Link from 'next/link'
 
 const actions = [
-  {
-    id: 'categories',
-    label: 'Categories',
-    items: [
-      {
-        name: 'Analytics',
-        description: 'Get a better understanding of your traffic',
-        href: '#',
-        icon: ChartPieIcon
-      },
-      {
-        name: 'Engagement',
-        description: 'Speak directly to your customers',
-        href: '#',
-        icon: CursorArrowRaysIcon
-      },
-      {
-        name: 'Integrations',
-        description: 'Connect with third-party tools',
-        href: '#',
-        icon: SquaresPlusIcon
-      },
-      {
-        name: 'Automations',
-        description: 'Build strategic funnels that will convert',
-        href: '#',
-        icon: ArrowPathIcon
-      }
-    ]
-  },
   {
     id: 'about',
     label: 'About',
@@ -74,14 +31,7 @@ export const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   const isAuthenticated = storeAuth((store) => store.isAuthenticated)
   const open = storeModalAuth((store) => store.open)
-  useEffect(() => {
-    const a = async () => {
-      const res = await getCategories()
-      console.log(res)
-      console.log(res.data)
-    }
-    a()
-  }, [])
+  const { categories } = useCategories()
   return (
     <header className='bg-white'>
       <nav
@@ -94,7 +44,12 @@ export const Header = () => {
             <WrenchScrewdriverIcon className='size-8' />
           </a>
         </div>
-        <div className='flex lg:hidden'>
+        <div className='flex items-center gap-2 lg:hidden'>
+          {isAuthenticated && (
+            <Link href='/dashboard'>
+              <UserCircleIcon className='size-7' />
+            </Link>
+          )}
           <button
             type='button'
             onClick={() => setMobileMenuOpen(true)}
@@ -105,70 +60,29 @@ export const Header = () => {
           </button>
         </div>
         <PopoverGroup className='hidden lg:flex lg:gap-x-8'>
-          {actions.map((action) => {
-            const actionItemsAmount = action.items?.length ?? 0
-            if (actionItemsAmount === 0) {
-              return (
-                <a
-                  key={action.id}
-                  href={action.to}
-                  className='text-sm/6 font-semibold text-gray-900'
-                >
-                  {action.label}
-                </a>
-              )
-            }
-            return (
-              <Popover className='relative' key={action.id}>
-                <PopoverButton className='flex outline-none items-center gap-x-1 text-sm/6 font-semibold text-gray-900'>
-                  {action.label}
-                  <ChevronDownIcon
-                    aria-hidden='true'
-                    className='size-5 flex-none text-gray-400'
-                  />
-                </PopoverButton>
-
-                <PopoverPanel
-                  transition
-                  className='absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in'
-                >
-                  <div className='p-4'>
-                    {action.items?.map((item) => (
-                      <div
-                        key={item.name}
-                        className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-gray-50'
-                      >
-                        <div className='flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white'>
-                          <item.icon
-                            aria-hidden='true'
-                            className='size-6 text-gray-600 group-hover:text-indigo-600'
-                          />
-                        </div>
-                        <div className='flex-auto'>
-                          <a
-                            href={item.href}
-                            className='block font-semibold text-gray-900'
-                          >
-                            {item.name}
-                            <span className='absolute inset-0' />
-                          </a>
-                          <p className='mt-1 text-gray-600'>
-                            {item.description}
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </PopoverPanel>
-              </Popover>
-            )
-          })}
+          <HeaderActionPopover
+            label={'Categories'}
+            items={categories.map((cat) => ({
+              name: cat.title,
+              description: cat.description,
+              href: `/categories/${cat.id}`
+            }))}
+          />
+          {actions.map((action) => (
+            <a
+              key={action.id}
+              href={action.to}
+              className='text-sm/6 font-semibold text-gray-900'
+            >
+              {action.label}
+            </a>
+          ))}
         </PopoverGroup>
         <div className='hidden lg:flex lg:items-center lg:gap-2 lg:flex-1 lg:justify-end'>
           {isAuthenticated ? (
-            <Button>
+            <Link href='/dashboard'>
               <UserCircleIcon className='size-8' />
-            </Button>
+            </Link>
           ) : (
             <>
               <Button
@@ -204,59 +118,46 @@ export const Header = () => {
             </button>
           </div>
           <div className='mt-6 flow-root'>
-            <div className='-my-6 divide-y divide-gray-500/10'>
+            <div
+              className={`-my-6 ${
+                !isAuthenticated && 'divide-y divide-gray-500/10'
+              }`}
+            >
               <div className='space-y-2 py-6'>
-                {actions.map((action) => {
-                  const actionItemsAmount = action.items?.length ?? 0
-                  if (actionItemsAmount === 0) {
-                    return (
-                      <a
-                        key={action.id}
-                        href={action.to}
-                        className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
-                      >
-                        {action.label}
-                      </a>
-                    )
-                  }
-                  return (
-                    <Disclosure as='div' className='-mx-3' key={action.id}>
-                      <DisclosureButton className='group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'>
-                        {action.label}
-                        <ChevronDownIcon
-                          aria-hidden='true'
-                          className='size-5 flex-none group-data-[open]:rotate-180'
-                        />
-                      </DisclosureButton>
-                      <DisclosurePanel className='mt-2 space-y-2'>
-                        {action.items?.map((item) => (
-                          <DisclosureButton
-                            key={item.name}
-                            as='a'
-                            href={item.href}
-                            className='block rounded-lg py-2 pl-6 pr-3 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50'
-                          >
-                            {item.name}
-                          </DisclosureButton>
-                        ))}
-                      </DisclosurePanel>
-                    </Disclosure>
-                  )
-                })}
+                <HeaderActionDisclosure
+                  label={'Categories'}
+                  items={categories.map((cat) => ({
+                    name: cat.title,
+                    href: `/categories/${cat.id}`
+                  }))}
+                />
+                {actions.map((action) => (
+                  <a
+                    key={action.id}
+                    href={action.to}
+                    className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
+                  >
+                    {action.label}
+                  </a>
+                ))}
               </div>
               <div className='space-y-2 py-6'>
-                <Button
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
-                  onClick={() => open('login')}
-                >
-                  Log in
-                </Button>
-                <Button
-                  className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
-                  onClick={() => open('login')}
-                >
-                  Sign up
-                </Button>
+                {!isAuthenticated && (
+                  <>
+                    <Button
+                      className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
+                      onClick={() => open('login')}
+                    >
+                      Log in
+                    </Button>
+                    <Button
+                      className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50'
+                      onClick={() => open('login')}
+                    >
+                      Sign up
+                    </Button>
+                  </>
+                )}
               </div>
             </div>
           </div>
