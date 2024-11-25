c473eb130d67891b941583680ededee4f43da8f3
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-25 07:12:36 -0600

    Create dashboard page

--- servi-connect/src/app/dashboard/page.tsx
+++ servi-connect/src/app/dashboard/page.tsx
@@ -1,12 +1,72 @@
 'use client'
-import { WrapperContentDelimiter, WrapperProtectedRoute } from '@/components'
+import {
+  ModalAddress,
+  WrapperContentDelimiter,
+  WrapperProtectedRoute
+} from '@/components'
+import { CxButton } from '@/components/Interactions'
+import { useUser } from '@/logic/hooks'
+import { storeAuth, storeModalAddress } from '@/logic/stores'
 
 export default function Dashboard() {
+  const open = storeModalAddress((store) => store.open)
+  const user = storeAuth((store) => store.user)
+  const { userAddress } = useUser()
   return (
     <WrapperProtectedRoute>
       <WrapperContentDelimiter>
-        <h1>PageDashboard</h1>
+        <span className='text-sm px-3 py-[2px] bg-orange-200 rounded-full border border-solid border-orange-500 mb-1 block w-max'>
+          {user?.role}
+        </span>
+        <h2 className='text-3xl font-semibold mb-3'>
+          Hi {user?.firstName} {user?.lastName}!
+        </h2>
+        <div className='mb-2'>
+          <span className='text-xs text-black/60'>Email:</span>
+          <p>{user?.email}</p>
+        </div>
+        <div className='mb-2'>
+          <span className='text-xs text-black/60'>Phone:</span>
+          <p>
+            +{user?.phoneNumberAreaCode} {user?.phoneNumber}
+          </p>
+        </div>
+        <div className='mb-2'>
+          <span className='text-xs text-black/60'>Description:</span>
+          <p>{user?.description}</p>
+        </div>
+        {userAddress && (
+          <>
+            <div className='mb-2'>
+              <span className='text-xs text-black/60'>Departamento:</span>
+              <p>
+                {userAddress?.countryDepartmentCity.countryDepartment.title}
+              </p>
+            </div>
+            <div className='mb-2'>
+              <span className='text-xs text-black/60'>Ciudad:</span>
+              <p>{userAddress?.countryDepartmentCity.title}</p>
+            </div>
+            <div className='mb-2'>
+              <span className='text-xs text-black/60'>Calle:</span>
+              <p>{userAddress?.street}</p>
+            </div>
+            <div className='mb-2'>
+              <span className='text-xs text-black/60'>Codigo postal:</span>
+              <p>{userAddress?.postalCode}</p>
+            </div>
+          </>
+        )}
+        <CxButton
+          onClick={() => {
+            if (userAddress) return open(userAddress)
+            open()
+          }}
+        >
+          {userAddress ? 'Actualizar' : 'Crear'} dirección
+        </CxButton>
       </WrapperContentDelimiter>
+      <ModalAddress />
     </WrapperProtectedRoute>
   )
 }
