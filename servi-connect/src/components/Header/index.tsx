08d2fb8bed978fbd20ce93c7e11911a3d2c310d8
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-25 01:13:33 -0600

    Replace anchor tags for link next components

--- servi-connect/src/components/Header/index.tsx
+++ servi-connect/src/components/Header/index.tsx
@@ -39,10 +39,10 @@ export const Header = () => {
         className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
       >
         <div className='flex lg:flex-1'>
-          <a href='#' className='-m-1.5 p-1.5'>
+          <Link href='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>ServiConnect</span>
             <WrenchScrewdriverIcon className='size-8' />
-          </a>
+          </Link>
         </div>
         <div className='flex items-center gap-2 lg:hidden'>
           {isAuthenticated && (
@@ -69,13 +69,13 @@ export const Header = () => {
             }))}
           />
           {actions.map((action) => (
-            <a
+            <Link
               key={action.id}
               href={action.to}
               className='text-sm/6 font-semibold text-gray-900'
             >
               {action.label}
-            </a>
+            </Link>
           ))}
         </PopoverGroup>
         <div className='hidden lg:flex lg:items-center lg:gap-2 lg:flex-1 lg:justify-end'>
@@ -104,10 +104,10 @@ export const Header = () => {
         <div className='fixed inset-0 z-10' />
         <DialogPanel className='fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10'>
           <div className='flex items-center justify-between'>
-            <a href='#' className='-m-1.5 p-1.5'>
+            <Link href='/' className='-m-1.5 p-1.5'>
               <span className='sr-only'>ServiConnect</span>
               <WrenchScrewdriverIcon className='size-8' />
-            </a>
+            </Link>
             <button
               type='button'
               onClick={() => setMobileMenuOpen(false)}
