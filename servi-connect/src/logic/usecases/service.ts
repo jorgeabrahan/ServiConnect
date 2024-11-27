ae0dae92ccae66cb21216c9c04a5edd91e09f427
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-27 02:46:08 -0600

    Create use case to create service request

--- servi-connect/src/logic/usecases/service.ts
+++ servi-connect/src/logic/usecases/service.ts
@@ -4,6 +4,18 @@ import { Service } from '../entities'
 import { API_URL } from 'config'
 import { toast } from 'sonner'
 
+interface ServiceRequestDto {
+  serviceId: string
+  description: string
+  date: string
+  time: string
+  hoursBooked: number
+  specificationAmounts: {
+    serviceSpecificationAmountId: string
+    value: number
+  }[]
+}
+
 export const getServiceById = async (id: string) => {
   try {
     const response = await axios.get<StandardApiResponse<Service>>(
@@ -18,6 +30,36 @@ export const getServiceById = async (id: string) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (_) {
     const fallbackError = 'Failed to get service by ID'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
+
+export const createServiceRequest = async (
+  serviceRequest: ServiceRequestDto,
+  userId: string,
+  jwt: string
+) => {
+  try {
+    const response = await axios.post<StandardApiResponse<Service>>(
+      `${API_URL}/service-requests/user/${userId}`,
+      serviceRequest,
+      {
+        headers: {
+          Authorization: `Bearer ${jwt}`
+        }
+      }
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    toast.success('Service request created successfully')
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to create service request'
     toast.error(fallbackError)
     return { isSuccess: false, data: null, error: fallbackError }
   }
