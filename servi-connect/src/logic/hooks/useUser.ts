96509ae469d2d4752fd3d7fb08cffc91ac74f0cb
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-27 22:26:24 -0600

    Load professional user profile only once from the use user hook and preserve it in store

--- servi-connect/src/logic/hooks/useUser.ts
+++ servi-connect/src/logic/hooks/useUser.ts
@@ -1,10 +1,17 @@
 import { useEffect } from 'react'
 import { storeAuth, storeUser } from '../stores'
 import { getUserAddress } from '../usecases/user'
+import { getProfessionalUser } from '../usecases'
 
 export const useUser = () => {
   const jwt = storeAuth((store) => store.jwt)
   const user = storeAuth((store) => store.user)
+  const userProfessionalProfile = storeUser(
+    (store) => store.userProfessionalProfile
+  )
+  const setUserProfessionalProfile = storeUser(
+    (store) => store.setUserProfessionalProfile
+  )
   const userAddress = storeUser((store) => store.userAddress)
   const setUserAddress = storeUser((store) => store.setUserAddress)
   useEffect(() => {
@@ -16,5 +23,15 @@ export const useUser = () => {
     }
     get()
   }, [jwt, user, userAddress, setUserAddress])
-  return { userAddress }
+  useEffect(() => {
+    if (userProfessionalProfile || !user || !jwt) return
+    const get = async () => {
+      const res = await getProfessionalUser(user.id, jwt)
+      if (!res.isSuccess) return
+      setUserProfessionalProfile(res.data ?? null)
+    }
+    get()
+  }, [])
+
+  return { userAddress, userProfessionalProfile }
 }
