dc08d7b2213bffb0060b215dba8cf42217089473
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-27 22:16:39 -0600

    Create professional user use cases to get, and create a professional user and to create a service area

---
+++ servi-connect/src/logic/usecases/professionalUser.ts
@@ -0,0 +1,87 @@
+import axios from 'axios'
+import { StandardApiResponse } from '../types'
+import { ProfessionalUser, ProfessionalUserServiceArea } from '../entities'
+import { API_URL } from 'config'
+import { toast } from 'sonner'
+
+interface ProfessionalUserServiceAreaDto {
+  countryDepartmentCityId: string
+}
+
+export const getProfessionalUser = async (userId: string, token: string) => {
+  try {
+    const response = await axios.get<StandardApiResponse<ProfessionalUser>>(
+      `${API_URL}/professional-users/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to get professional user'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
+
+export const createProfessionalUser = async (userId: string, token: string) => {
+  try {
+    const response = await axios.post<StandardApiResponse<ProfessionalUser>>(
+      `${API_URL}/professional-users/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    )
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    toast.success('Professional user created successfully')
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_) {
+    const fallbackError = 'Failed to create professional user'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
+
+export const createProfessionalUserServiceArea = async (
+  userId: string,
+  body: ProfessionalUserServiceAreaDto,
+  token: string
+) => {
+  try {
+    const response = await axios.post<
+      StandardApiResponse<ProfessionalUserServiceArea>
+    >(`${API_URL}/professional-users/${userId}/service-areas`, body, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    const { isSuccess, data: responseData, error } = response.data
+    if (!isSuccess || !responseData) {
+      toast.error(error)
+      return response.data
+    }
+    toast.success('Service area added successfully')
+    return response.data
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    console.log(error)
+    const fallbackError = 'Failed to add service area to professional user'
+    toast.error(fallbackError)
+    return { isSuccess: false, data: null, error: fallbackError }
+  }
+}
