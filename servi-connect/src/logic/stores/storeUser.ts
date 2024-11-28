f41ce6d94e1ddbe71dc81ef3bf29ac8faf77fcba
jorgeabrahan <jorge24abrahan@gmail.com>
2024-11-27 22:21:07 -0600

    Add properties and methods to store the professional user profile info in the user store, also add methods to add and remove a professional user service area

--- servi-connect/src/logic/stores/storeUser.ts
+++ servi-connect/src/logic/stores/storeUser.ts
@@ -1,14 +1,27 @@
 import { create } from 'zustand'
-import { UserAddress } from '../entities/UserAddress'
+import {
+  ProfessionalUser,
+  ProfessionalUserServiceArea,
+  UserAddress
+} from '../entities'
 
 interface StoreUser {
   userAddress: UserAddress | null
+  userProfessionalProfile: ProfessionalUser | null
   setUserAddress: (userAddress: UserAddress | null) => void
   updateUserAddress: (userAddress: Omit<UserAddress, 'id' | 'user'>) => void
+  setUserProfessionalProfile: (
+    userProfessionalProfile: ProfessionalUser | null
+  ) => void
+  addUserProfessionalServiceArea: (
+    serviceArea: ProfessionalUserServiceArea
+  ) => void
+  removeUserProfessionalServiceArea: (serviceAreaId: string) => void
 }
 
 export const storeUser = create<StoreUser>((set) => ({
   userAddress: null,
+  userProfessionalProfile: null,
   setUserAddress: (userAddress) => set({ userAddress }),
   updateUserAddress: (userAddressDto) =>
     set((state) => {
@@ -21,6 +34,37 @@ export const storeUser = create<StoreUser>((set) => ({
           neighborhood: userAddressDto.neighborhood,
           postalCode: userAddressDto.postalCode,
           countryDepartmentCity: userAddressDto.countryDepartmentCity
+        }
+      }
+    }),
+  setUserProfessionalProfile: (userProfessionalProfile) =>
+    set({ userProfessionalProfile }),
+  addUserProfessionalServiceArea: (serviceArea) =>
+    set((state) => {
+      if (!state.userProfessionalProfile) return state
+      return {
+        ...state,
+        userProfessionalProfile: {
+          ...state.userProfessionalProfile,
+          professionalUserServiceAreas: [
+            ...(state.userProfessionalProfile?.professionalUserServiceAreas ??
+              []),
+            serviceArea
+          ]
+        }
+      }
+    }),
+  removeUserProfessionalServiceArea: (serviceAreaId) =>
+    set((state) => {
+      if (!state.userProfessionalProfile) return state
+      return {
+        ...state,
+        userProfessionalProfile: {
+          ...state.userProfessionalProfile,
+          professionalUserServiceAreas:
+            state.userProfessionalProfile.professionalUserServiceAreas.filter(
+              (serviceArea) => serviceArea.id !== serviceAreaId
+            )
         }
       }
     })
